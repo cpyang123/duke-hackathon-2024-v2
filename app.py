d459a0861a2c46c05f4f2b5098f860834f8fa577
Peter Yang <cpyang@umich.edu>
2024-11-03 01:13:14 -0500

    debugging

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ import time
 from PIL import Image
 import requests
 from io import BytesIO
+from bs4 import BeautifulSoup
+import re
+import json
 
 load_dotenv()
 
@@ -41,6 +44,35 @@ rag = LightRAG(
     llm_model_max_async=1
 )
 
+def get_profile_picture(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find the image tag containing the profile picture
+        # This depends on the website's HTML structure. 
+        # You might need to adjust the selectors below.
+        img_tag = soup.find('img', class_='img xs:max-w-[240px] md:max-w-full') 
+
+        # Extract the image URL
+        if img_tag:
+            img_url = img_tag['src']
+            try:
+                response = requests.get(img_url)
+                status_code = response.status_code
+            except:
+                status_code = 404
+            if status_code == 200:
+                return Image.open(BytesIO(response.content))
+            else:
+                return None
+    else:
+        return None
+
 # Load chat history from shelve file
 def load_chat_history():
     with shelve.open("chat_history") as db:
@@ -52,41 +84,39 @@ def save_chat_history(messages):
         db["messages"] = messages
 
 # Display a professor card
-def display_professor_card(name, university, research_areas, profile_url):
-    profile_image_url = False
+def display_professor_card(name, university, department, research_areas, profile_url):
     with st.container():
         st.markdown("---")
         cols = st.columns([1, 2])
         
         # Display profile image
         with cols[0]:
-            if profile_image_url:
-                response = requests.get(profile_image_url)
-                image = Image.open(BytesIO(response.content))
-                st.image(image, width=100)
+            if profile_url:
+                image = get_profile_picture(profile_url)
+                if image:
+                    st.image(image, width=200)
+                else:
+                    st.image("./static/duke_devil_logo.png", width = 200)
             else:
-                st.write("No image available")
+                st.image("./static/duke_devil_logo.png", width = 200)
         
         # Display professor details
         with cols[1]:
             st.markdown(f"**{university}**")
             st.markdown(f"### Prof. {name}")
+            st.markdown(f"#### Department: {department}")
             st.markdown(f"**Researches:** {research_areas}")
-            st.link_button("View profile", url=profile_url)
+            st.link_button("View profile", url= profile_url if profile_url != "" else "https://scholars.duke.edu/")
 
 # Initialize or load chat history
 if "messages" not in st.session_state:
     st.session_state.messages = load_chat_history()
 
-# File uploader for resume/CV
-uploaded_file = st.file_uploader('Upload your resume/cv here:', type="pdf")
-if uploaded_file is not None:
-    file_elements = partition(file=uploaded_file)
-    processed_file = "This should be a special case for the embedding. Make a special graph attribute as 'Me': \n" + "\n".join([str(i) for i in file_elements])
-    rag.insert(processed_file)
+
 
 # Sidebar with a button to delete chat history
 with st.sidebar:
+    st.markdown("---")
     if st.button("Delete Chat History"):
         st.session_state.messages = []
         save_chat_history([])
@@ -105,12 +135,19 @@ with st.sidebar:
             except Exception as e:
                 print(f"Error deleting {file_path}: {e}")
         rag.insert(text_content.decode('utf-8'))
+    # File uploader for resume/CV
+    uploaded_file = st.file_uploader('Upload your resume/cv here:', type="pdf")
+    if uploaded_file is not None:
+        file_elements = partition(file=uploaded_file)
+        processed_file = "This is 'my' profile: \n" + "\n".join([str(i) for i in file_elements])
+        rag.insert(processed_file)
 
 # Display chat messages
 for message in st.session_state.messages:
     avatar = USER_AVATAR if message["role"] == "user" else BOT_AVATAR
     with st.chat_message(message["role"], avatar=avatar):
         st.markdown(message["content"])
+
 
 # Main chat interface
 if prompt := st.chat_input("How can I help with your research?"):
@@ -136,18 +173,19 @@ if prompt := st.chat_input("How can I help with your research?"):
 
         
         sample_dict_string = """
-        {"professors" : [{"name" : "Chris P. Bacon", "university": "University of Breakfast", "research_areas": "area", "profile_url": "https://scholars.duke.edu/person/adam.brekke"}, 
-                                             {"name" : "Sun E. Sideup", "university": "University of Dinner", "research_areas": "area", "profile_url": "https://scholars.duke.edu/person/adam.brekke"}]}
+        {"professors" : [{"name" : "Chris P. Bacon", "university": "University of Breakfast", "department" : "Department of Mathematics", "research_areas": "area", "profile_url": "https://scholars.duke.edu/person/adam.brekke"}, 
+                                             {"name" : "Sun E. Sideup", "university": "University of Dinner", "department": "Department of Physics", "research_areas": "area", "profile_url": "https://scholars.duke.edu/person/adam.brekke"}]}
         """
-            
-        prof_dict_string = rag.query("If the below response includes professors, fetch the corresponding professors and return a string in the format of "+ sample_dict_string + rag_response, param=QueryParam(mode="hybrid"))
+        with st.spinner("Fetching Prof Profiles..."):
+            prof_dict_string = rag.query("If the below response includes professors, fetch the data for the professors and return a string in the format of: ```json " + sample_dict_string  + "``` use empty string if there fields missing. " + rag_response, param=QueryParam(mode="hybrid"))
         # Process response to extract professor details (assuming JSON or structured format)
         try:
-            response_data = eval(prof_dict_string[8:-3])
+            response_data = json.loads(prof_dict_string[8:-3]) 
             for prof in response_data.get("professors", []):
                 professor_data.append({
                     "name": prof["name"],
                     "university": prof["university"],
+                    "department": prof["department"],
                     "research_areas": prof["research_areas"],
                     "profile_url": prof.get("profile_url")
                 })
@@ -160,6 +198,7 @@ if prompt := st.chat_input("How can I help with your research?"):
             display_professor_card(
                 name=prof["name"],
                 university=prof["university"],
+                department=prof["department"],
                 research_areas=prof["research_areas"],
                 profile_url=prof["profile_url"]
             )
--- entry_point.py
+++ entry_point.py
@@ -1,9 +1,19 @@
 # This is the entry point
-
 import streamlit as st
 
-create_page = st.Page("graph_page.py", title="Relationship Visualization")
-delete_page = st.Page("app.py", title="Main Chat")
+page1 = st.Page("app.py", title="Prof-Finder")
+page2 = st.Page("graph_page1.py", title="Duke ProfGraph")
 
-pg = st.navigation([delete_page, create_page])
+pg = st.navigation([page1, page2], position = "hidden")
+
+with st.sidebar:
+    st.subheader("ProfMatch Tools")
+    st.page_link(page1, label = page1.title)
+    st.page_link(page2, label = page2.title)
+    
+    st.subheader("External Resources")
+    st.page_link(
+        page = "https://scholars.duke.edu/", label = "Scholars@Duke"
+    )
+    
 pg.run()
